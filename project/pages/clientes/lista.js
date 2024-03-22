02f982823583449d5a2b9836bb4fe90f65d689bd
Thauan Santos <126874939+Thauan-S@users.noreply.github.com>
2024-03-22 17:25:32 -0300

    update clients list

--- project/pages/clientes/lista.js
+++ project/pages/clientes/lista.js
@@ -2,14 +2,23 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Table from "@/components/table";
 import HeadComponent from "@/components/head";
+import { headers } from "@/next.config";
 const Clientes = () => {
   const [clientes, setClientes] = useState(null);
+  const [username,setUsername]=useState()
   useEffect(() => {
+    setUsername(window.localStorage.getItem('username'))
     axios
-      .get("https://localhost:7250/api/Clientes")
+      .get("http://localhost:80/api/clientes/v1",{
+        headers:{
+          Authorization:`Bearer ${window.localStorage.getItem('token')}`
+        }
+        
+      })
       .then((response) => {
-        setClientes(response.data);
         console.log("Response", response.data);
+        setClientes(response.data._embedded.clienteVOList);
+        
       })
       .catch((error) => {
         console.error("Erro ao Listar os Clientes", error);
@@ -19,6 +28,7 @@ const Clientes = () => {
   return (
     <main>
       <HeadComponent title={" Clientes | Lista"} />
+      <h1 className="text-center">Bem vindo ,{username}</h1>
       <Table clientes={clientes} />
     </main>
   );
