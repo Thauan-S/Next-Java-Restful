14d9df7357128ac9b1052db8906c27f5de1f3718
Thauan Santos <126874939+Thauan-S@users.noreply.github.com>
2024-03-22 17:26:56 -0300

    update delete

--- project/pages/clientes/delete-cliente/[codigo].js
+++ project/pages/clientes/delete-cliente/[codigo].js
@@ -1,45 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
 const DeleteCliente = () => {
   const router = useRouter();
   const { codigo } = router.query;
   const [clienteId, setClienteId] = useState(codigo);
-
+  const [token, setToken] = useState();
+  useEffect(()=>{
+    setToken(window.localStorage.getItem("token"))
+  },[])
   const handleDeleteClient = () => {
+   
     axios
-      .delete("https://localhost:7250/api/Clientes/"+clienteId)
-      .then(() => {
+      .delete("http://localhost:80/api/clientes/v1/" + clienteId, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
         router.push("/clientes/lista");
       })
       .catch((error) => {
         console.error("não foi possível excluir o cliente", clienteId, error);
       });
+    
   };
   return (
     <main>
-      <h1>Cliente a ser excluido</h1>
-      <table>
-        <tr>
-          <td>
-            <label>Id do cliente a ser excluido:</label>
-          </td>
-          <td>
+      <h1>Cliente a ser excluído</h1>
             <input
               type="text"
+              readOnly
               value={clienteId}
               onChange={(e) => setClienteId(e.target.value)}
             />
-          </td>
-        </tr>
-        <tr>
-          <td>
             <button className="btn btn-primary" onClick={handleDeleteClient}>
               excluir
             </button>
-          </td>
-        </tr>
-      </table>
+         
     </main>
   );
 };
