2236c54f666c2862daa32aa957aff99048229e38
Thauan Santos <126874939+Thauan-S@users.noreply.github.com>
2024-03-28 08:18:54 -0300

    update

--- project/hooks/useGetClients.js
+++ project/hooks/useGetClients.js
@@ -1,14 +1,44 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 function useGetClients() {
-  const [username, setUsername] = useState();
+  const [username, setUsername] = useState(" ");
   const [clientes, setClientes] = useState(null);
+  var [page, setPage] = useState(1);
+  const [size, setSize] = useState(10);
+  const [direction, setDirection] = useState("asc");
+  const [getClients, setGetClients] = useState(() => {
+    return (p)=>{
+    axios
+      .get("http://localhost:80/api/clientes/v1", {
+        headers: {
+          Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+        },
+        params: {
+          page: p,
+          size: size,
+          direction: direction,
+        },
+      })
+      .then((response) => {
+        console.log("Response", response.data);
+        setClientes(response.data._embedded.clienteVOList);
+      })
+      .catch((error) => {
+        console.error("Erro ao Listar os Clientes", error);
+      })
+    }
+  });
   useEffect(() => {
     setUsername(window.localStorage.getItem("username"));
     axios
       .get("http://localhost:80/api/clientes/v1", {
         headers: {
           Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+        },
+        params: {
+          page: page,
+          size: size,
+          direction: direction,
         },
       })
       .then((response) => {
@@ -19,7 +49,7 @@ function useGetClients() {
         console.error("Erro ao Listar os Clientes", error);
       });
   }, []);
-  return { username,clientes };
+  return { username, clientes, setClientes, setPage, page, getClients };
 }
 
 export default useGetClients;
