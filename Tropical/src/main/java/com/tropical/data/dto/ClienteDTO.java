c21902e8654b3ab21202fc69a75c268ae7db49f4
Thauan Santos <126874939+Thauan-S@users.noreply.github.com>
2024-04-12 09:31:27 -0300

    Dto para criação do cliente

--- Tropical/src/main/java/com/tropical/data/dto/ClienteDTO.java
+++ Tropical/src/main/java/com/tropical/data/dto/ClienteDTO.java
@@ -1,9 +1,12 @@
 package com.tropical.data.dto;
 
 import java.util.Date;
+import java.util.List;
 
 import com.tropical.model.User;
 
-public record ClienteDTO(String nome, String telefone, Date dataNascimento,String cep,User user) {
+public record ClienteDTO( String nome, String telefone, Date dataNascimento,String cep,User user) {
+
+	
 
 }
