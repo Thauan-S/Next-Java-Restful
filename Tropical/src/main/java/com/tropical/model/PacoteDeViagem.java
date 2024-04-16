6dab649e0f8ba0e137a2d077113be46b8e4bb6b4
Thauan Santos <126874939+Thauan-S@users.noreply.github.com>
2024-04-16 20:38:57 -0300

    criação da classe pacote de viagem

---
+++ Tropical/src/main/java/com/tropical/model/PacoteDeViagem.java
@@ -0,0 +1,142 @@
+package com.tropical.model;
+
+import java.math.BigDecimal;
+
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.format.annotation.NumberFormat.Style;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity(name = "pacote_de_viagem")
+public class PacoteDeViagem {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_pacote")
+	private Long id;
+
+	@Column(length = 45, nullable = false)
+	private String destino;
+
+	@Column(length = 150, nullable = false)
+	private String descricao;
+
+	@Column(length = 150, nullable = false)
+	private String categoria;
+
+	@Column(name = "duracao_dias", nullable = false)
+	private int duracaoEmDias;
+
+	@Column(nullable = false, length = 300)
+	private String imagem;
+
+	@Column(nullable = false)
+	@NumberFormat(style = Style.CURRENCY, pattern = "#,##0.00")
+	private BigDecimal preco;
+	
+	@ManyToOne
+	@JoinColumn(name="empresa_id")
+	private Empresa empresa;
+	
+	
+	
+
+	public PacoteDeViagem() {
+
+	}
+
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+
+	public String getDestino() {
+		return destino;
+	}
+
+
+
+
+	public void setDestino(String destino) {
+		this.destino = destino;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+
+	public int getDuracaoEmDias() {
+		return duracaoEmDias;
+	}
+
+
+	public void setDuracaoEmDias(int duracaoEmDias) {
+		this.duracaoEmDias = duracaoEmDias;
+	}
+
+
+	public String getImagem() {
+		return imagem;
+	}
+
+
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+
+	public Empresa getEmpresa() {
+		return empresa;
+	}
+
+
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
+	}
+
+	
+}
