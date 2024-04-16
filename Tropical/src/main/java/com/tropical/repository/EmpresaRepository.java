a154d3f846ce217cd1ad325a32433d4d6f735fde
Thauan Santos <126874939+Thauan-S@users.noreply.github.com>
2024-04-16 20:39:44 -0300

    criação do EmpresaRepository

---
+++ Tropical/src/main/java/com/tropical/repository/EmpresaRepository.java
@@ -0,0 +1,18 @@
+package com.tropical.repository;
+
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tropical.model.Empresa;
+import com.tropical.model.User;
+
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+	Optional<Empresa> findByUser(User user);
+	
+//	@Query("SELECT c  FROM Cliente c WHERE c.nome LIKE LOWER(CONCAT ('%',:nome,'%'))")
+//	Page<Cliente> findClientesByName(@Param("nome")String nome,Pageable pageable);
+}
