db98e3e47c1abf976c126ceb30eb6128ff11e5e2
Thauan Santos <126874939+Thauan-S@users.noreply.github.com>
2024-04-16 20:37:17 -0300

    adição da criação de nova empresa já com o seu tipo de usuário: empresa

--- Tropical/src/main/java/com/tropical/controller/UserController.java
+++ Tropical/src/main/java/com/tropical/controller/UserController.java
@@ -13,9 +13,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tropical.data.dto.CreateUserDto;
+import com.tropical.data.dto.EmpresaDTO;
 import com.tropical.exceptions.UserAlreadyExistsException;
+import com.tropical.exceptions.UserAlreadyUsedException;
+import com.tropical.model.Empresa;
 import com.tropical.model.Role;
 import com.tropical.model.User;
+import com.tropical.repository.EmpresaRepository;
 import com.tropical.repository.RoleRepository;
 import com.tropical.repository.UserRepository;
 
@@ -25,12 +29,14 @@ public class UserController {
 	private final UserRepository userRepository;
 	private final RoleRepository roleRepository;
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+	private final EmpresaRepository empresaRepository;
 
 	public UserController(UserRepository userRepository, RoleRepository roleRepository,
-			BCryptPasswordEncoder bCryptPasswordEncoder) {
+			BCryptPasswordEncoder bCryptPasswordEncoder,EmpresaRepository empresaRepository) {
 		this.userRepository = userRepository;
 		this.roleRepository = roleRepository;
 		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+		this.empresaRepository = empresaRepository;
 	}
 
 	@PostMapping("/register/user")
@@ -61,6 +67,33 @@ public class UserController {
 		user.setPassword(bCryptPasswordEncoder.encode(createUserDto.password()));
 		user.setRoles(Set.of(adminRole));
 		userRepository.save(user);
+		return ResponseEntity.ok().build();
+	}
+	@PostMapping("/register/empresa")
+	public ResponseEntity<Void> newempresa(@RequestBody EmpresaDTO empresaDTO) {
+		var companyRole=roleRepository.findByName(Role.Values.EMPRESA.name());
+		var userDB= userRepository.findByUsername(empresaDTO.getUser().getUsername());
+		if(userDB.isPresent()) {
+			throw new UserAlreadyExistsException();
+		}
+		var user= new User();
+		user.setUsername(empresaDTO.getUser().getUsername());
+		user.setPassword(bCryptPasswordEncoder.encode(empresaDTO.getUser().getPassword()));
+		user.setRoles(Set.of(companyRole));
+		userRepository.save(user);
+		
+	Empresa empresa= new Empresa();
+		
+		if(empresaRepository.findByUser(user).isEmpty()) {
+			empresa.setUser(user);
+			empresa.setNomeEmpresa(empresaDTO.getNomeEmpresa());
+			empresa.setCnpj(empresaDTO.getCnpj());
+			empresa.setEndereco(empresaDTO.getEndereco());
+			empresaRepository.save(empresa);
+		}
+		else {
+			throw new UserAlreadyUsedException();
+		}
 		return ResponseEntity.ok().build();
 	}
 
