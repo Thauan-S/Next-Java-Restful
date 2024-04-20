5b2ba977473835d97f05c5561bb3c6645d51807d
Thauan Santos <126874939+Thauan-S@users.noreply.github.com>
2024-04-20 17:20:34 -0300

    comentário da criação de user admin

--- Tropical/src/main/java/com/tropical/config/AdminUserConfig.java
+++ Tropical/src/main/java/com/tropical/config/AdminUserConfig.java
@@ -1,53 +1,53 @@
-package com.tropical.config;
-
-
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.tropical.model.Role;
-import com.tropical.model.User;
-import com.tropical.repository.RoleRepository;
-import com.tropical.repository.UserRepository;
-
-import java.util.Set;
-
-@Configuration
-public class AdminUserConfig implements CommandLineRunner {
-
-    private RoleRepository roleRepository;
-    private UserRepository userRepository;
-    private BCryptPasswordEncoder passwordEncoder;
-
-    public AdminUserConfig(RoleRepository roleRepository,
-                           UserRepository userRepository,
-                           BCryptPasswordEncoder passwordEncoder) {
-        this.roleRepository = roleRepository;
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    @Override
-    @Transactional
-    public void run(String... args) throws Exception {
-
-        var roleAdmin = roleRepository.findByName(Role.Values.ADMIN.name());
-
-        var userAdmin = userRepository.findByUsername("admin");
-
-        userAdmin.ifPresentOrElse(
-                user -> {
-                    System.out.println("admin ja existe");
-                },
-                () -> {
-                    var user = new User();
-                    user.setUsername("admin");
-                    user.setPassword(passwordEncoder.encode("123"));
-                    user.setRoles(Set.of(roleAdmin));
-                    userRepository.save(user);
-                }
-        );
-    }
-}
+//package com.tropical.config;
+//
+//
+//
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import com.tropical.model.Role;
+//import com.tropical.model.User;
+//import com.tropical.repository.RoleRepository;
+//import com.tropical.repository.UserRepository;
+//
+//import java.util.Set;
+//
+//@Configuration
+//public class AdminUserConfig implements CommandLineRunner {
+//
+//    private RoleRepository roleRepository;
+//    private UserRepository userRepository;
+//    private BCryptPasswordEncoder passwordEncoder;
+//
+//    public AdminUserConfig(RoleRepository roleRepository,
+//                           UserRepository userRepository,
+//                           BCryptPasswordEncoder passwordEncoder) {
+//        this.roleRepository = roleRepository;
+//        this.userRepository = userRepository;
+//        this.passwordEncoder = passwordEncoder;
+//    }
+//
+//    @Override
+//    @Transactional
+//    public void run(String... args) throws Exception {
+//
+//        var roleAdmin = roleRepository.findByName(Role.Values.ADMIN.name());
+//
+//        var userAdmin = userRepository.findByUsername("admin");
+//
+//        userAdmin.ifPresentOrElse(
+//                user -> {
+//                    System.out.println("admin ja existe");
+//                },
+//                () -> {
+//                    var user = new User();
+//                    user.setUsername("admin");
+//                    user.setPassword(passwordEncoder.encode("123"));
+//                    user.setRoles(Set.of(roleAdmin));
+//                    userRepository.save(user);
+//                }
+//        );
+//    }
+//}
