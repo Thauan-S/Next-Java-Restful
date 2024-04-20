c315980b3f2aa2fd107cde65ead6b8d1e6087923
Thauan Santos <126874939+Thauan-S@users.noreply.github.com>
2024-04-20 17:30:12 -0300

    criação do ReservaRepository

---
+++ Tropical/src/main/java/com/tropical/repository/ReservaRepository.java
@@ -0,0 +1,13 @@
+package com.tropical.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tropical.model.Reserva;
+
+
+public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+	
+	
+	
+}
