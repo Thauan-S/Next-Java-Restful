e667f4d768a5670d734900a0b3ed5965dcb7d7ab
Thauan Santos <126874939+Thauan-S@users.noreply.github.com>
2024-04-27 15:21:41 -0300

    indentanção de código

--- Tropical/src/main/java/com/tropical/controller/ClienteController.java
+++ Tropical/src/main/java/com/tropical/controller/ClienteController.java
@@ -35,80 +35,55 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-
 @RestController
 @RequestMapping("/api/clientes/v1")
-@Tag(name = "Clientes" ,description = "Endpoint para Gerenciar Clientes")
+@Tag(name = "Clientes", description = "Endpoint para Gerenciar Clientes")
 public class ClienteController {
-	
-private final ClienteRepository clienteRepository;
-private final UserRepository userRepository;
-public ClienteController(ClienteRepository clienteRepository,UserRepository userRepository) {
-this.clienteRepository = clienteRepository;
-this.userRepository = userRepository;
-}
-	
-	@GetMapping(value="/{id}",
-			produces = { 	MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML,MediaType.APPLICATION_YML})
-	
-	@Operation(
-			summary = "Busca um cliente",
-			description = "Busca um cliente",
-			tags = {"Clientes"},
-			responses = {
-				@ApiResponse(description="Success",responseCode ="200",
-						content=@Content(schema = @Schema(implementation = ClienteDto.class))
-				),
-				@ApiResponse(description="No Content",responseCode ="204",content = @Content ),
-				@ApiResponse(description="Bad Request",responseCode ="400",content = @Content ),
-				@ApiResponse(description="Unauthorized ",responseCode ="401",content = @Content ),
-				@ApiResponse(description="Not Found",responseCode ="404",content = @Content),
-				@ApiResponse(description="Internal Server Error",responseCode ="500",content = @Content )
-				}
-			)
-	@PreAuthorize(value ="hasAuthority('SCOPE_ADMIN')")
-	public ResponseEntity<ClienteDto> findById(@PathVariable Long id ) {
-		System.out.println("esse é o id"+id);
-		var cliente= clienteRepository.findById(id).orElseThrow(()-> new NotFoundException("O cliente de id"+id+"não existe na base de dados"));
-		
-		return ResponseEntity.ok(new ClienteDto(cliente));
-		
+
+	private final ClienteRepository clienteRepository;
+	private final UserRepository userRepository;
+
+	public ClienteController(ClienteRepository clienteRepository, UserRepository userRepository) {
+		this.clienteRepository = clienteRepository;
+		this.userRepository = userRepository;
 	}
-	@GetMapping(
-			produces = { MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML,MediaType.APPLICATION_YML})
-	@Operation(
-			summary = "Busca todos clientes",
-			description = "Busca todos clientes",
-			tags = {"Clientes"},
-			responses = {
-				@ApiResponse(description="Success",responseCode ="200",content = {
-						@Content(
-								mediaType = "application/json",
-								array = @ArraySchema(schema = @Schema(implementation = ClienteDto.class))
-						)
-				}),
-				@ApiResponse(description="Bad Request",responseCode ="400",content = @Content ),
-				@ApiResponse(description="Unauthorized ",responseCode ="401",content = @Content ),
-				@ApiResponse(description="Not Found",responseCode ="404",content = @Content),
-				@ApiResponse(description="Internal Server Error",responseCode ="500",content = @Content )
-				}
-			)
-	
-	@PreAuthorize(value ="hasAuthority('SCOPE_ADMIN')")
-	public ResponseEntity<ClientListDto> findAll(
-			@RequestParam (value="page",defaultValue="0")int page,
-			@RequestParam (value="size",defaultValue="12")int size,
-			@RequestParam(value="direction",defaultValue="ASC")String direction){
-		var clientes =clienteRepository.findAll(PageRequest.of(page, size,Direction.valueOf(direction) ,"nome"))
-				.map(cliente -> new ClienteItemDTO(
-						cliente.getClienteId(),
-						cliente.getNome(),
-						cliente.getTelefone(),
-						cliente.getDataNascimento(),
-						cliente.getCep(),
-						cliente.getUser()));	
-		
-		return ResponseEntity.ok(new ClientListDto(clientes.getContent(), page,size,clientes.getTotalPages(),clientes.getTotalElements()));
+
+	@GetMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
+			MediaType.APPLICATION_YML })
+
+	@Operation(summary = "Busca um cliente", description = "Busca um cliente", tags = { "Clientes" }, responses = {
+			@ApiResponse(description = "Success", responseCode = "200", content = @Content(schema = @Schema(implementation = ClienteDto.class))),
+			@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized ", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content) })
+	@PreAuthorize(value = "hasAuthority('SCOPE_ADMIN')")
+	public ResponseEntity<ClienteDto> findById(@PathVariable Long id) {
+		var cliente = clienteRepository.findById(id)
+				.orElseThrow(() -> new NotFoundException("O cliente de id" + id + "não existe na base de dados"));
+		return ResponseEntity.ok(new ClienteDto(cliente));
+	}
+
+	@GetMapping(produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
+	@Operation(summary = "Busca todos clientes", description = "Busca todos clientes", tags = {
+			"Clientes" }, responses = { @ApiResponse(description = "Success", responseCode = "200", content = {
+					@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = ClienteDto.class))) }),
+					@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+					@ApiResponse(description = "Unauthorized ", responseCode = "401", content = @Content),
+					@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+					@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content) })
+
+	@PreAuthorize(value = "hasAuthority('SCOPE_ADMIN')")
+	public ResponseEntity<ClientListDto> findAll(@RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "size", defaultValue = "12") int size,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		var clientes = clienteRepository.findAll(PageRequest.of(page, size, Direction.valueOf(direction), "nome"))
+				.map(cliente -> new ClienteItemDTO(cliente.getClienteId(), cliente.getNome(), cliente.getTelefone(),
+						cliente.getDataNascimento(), cliente.getCep(), cliente.getUser()));
+
+		return ResponseEntity.ok(new ClientListDto(clientes.getContent(), page, size, clientes.getTotalPages(),
+				clientes.getTotalElements()));
 	}
 //	@GetMapping(
 //			value="/findClientesByName/{nome}",
@@ -140,34 +115,28 @@ this.userRepository = userRepository;
 //			.map(cliente -> new ClienteItemDTO(cliente.getClienteId(),cliente.ge
 //	
 //	}
-	
 
-	@PutMapping(
-			produces ={ MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML,MediaType.APPLICATION_YML},
-			consumes = { MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML,MediaType.APPLICATION_YML})
-	@Operation(
-			summary = "Atualiza um cliente",
-			description = "Atualiza um cliente através de JSON,XML ou YML",
-			tags = {"Clientes"},
-			responses = {
-				@ApiResponse(description="Updated",responseCode ="200",
-						content=@Content(schema = @Schema(implementation = ClienteDto.class))
-				),
-				
-				@ApiResponse(description="Bad Request",responseCode ="400",content = @Content ),
-				@ApiResponse(description="Unauthorized ",responseCode ="401",content = @Content ),
-				@ApiResponse(description="Not Found",responseCode ="404",content = @Content),
-				@ApiResponse(description="Internal Server Error",responseCode ="500",content = @Content )
-				}
-			)
+	@PutMapping(produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
+			MediaType.APPLICATION_YML }, consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
+					MediaType.APPLICATION_YML })
+	@Operation(summary = "Atualiza um cliente", description = "Atualiza um cliente através de JSON,XML ou YML", tags = {
+			"Clientes" }, responses = {
+					@ApiResponse(description = "Updated", responseCode = "200", content = @Content(schema = @Schema(implementation = ClienteDto.class))),
+
+					@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+					@ApiResponse(description = "Unauthorized ", responseCode = "401", content = @Content),
+					@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+					@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content) })
 	@PreAuthorize("hasAnyAuthority('SCOPE_ADMIN', 'SCOPE_BASIC')")
-	public ClienteDto update(@RequestBody ClienteDto clientedto,JwtAuthenticationToken token) throws AccessDeniedException {
-		var user=userRepository.findById( UUID.fromString(token.getName()));
-		var clibd=clienteRepository.findById(clientedto.getClienteId()).orElseThrow(()->new ResourceNotFoundException("Cliente de id "+clientedto.getClienteId()+" Não encontrado na base de dados"));
-		var isAdmin=user.get().getRoles()
-					.stream()
-					.anyMatch(role-> role.getName().equalsIgnoreCase(Role.Values.ADMIN.name()));
-		if(isAdmin||clibd.getUser().getUserId().equals(UUID.fromString(token.getName()))) {
+	public ClienteDto update(@RequestBody ClienteDto clientedto, JwtAuthenticationToken token)
+			throws AccessDeniedException {
+		var user = userRepository.findById(UUID.fromString(token.getName()));
+		var clibd = clienteRepository.findById(clientedto.getClienteId())
+				.orElseThrow(() -> new ResourceNotFoundException(
+						"Cliente de id " + clientedto.getClienteId() + " Não encontrado na base de dados"));
+		var isAdmin = user.get().getRoles().stream()
+				.anyMatch(role -> role.getName().equalsIgnoreCase(Role.Values.ADMIN.name()));
+		if (isAdmin || clibd.getUser().getUserId().equals(UUID.fromString(token.getName()))) {
 			user.get().setUsername(clientedto.getUser().getUsername());
 			user.get().setPassword(clientedto.getUser().getPassword());
 			clibd.setNome(clientedto.getNome());
@@ -176,38 +145,31 @@ this.userRepository = userRepository;
 			clibd.setCep(clientedto.getCep());
 			clienteRepository.save(clibd);
 			return new ClienteDto(clibd);
-		}else {
+		} else {
 			throw new ForbiddenAccesException("O usuário não tem permissão para realizar esta ação");
-		}		
+		}
 	}
+
 	@DeleteMapping("/{id}")
-	@Operation(
-			summary = "Deleta um cliente",
-			description = "Deleta um cliente",
-			tags = {"Clientes"},
-			responses = {
-				@ApiResponse(description="No Content",responseCode ="204",
-					content=@Content
-				),
-				@ApiResponse(description="Bad Request",responseCode ="400",content = @Content ),
-				@ApiResponse(description="Unauthorized ",responseCode ="401",content = @Content ),
-				@ApiResponse(description="Not Found",responseCode ="404",content = @Content),
-				@ApiResponse(description="Internal Server Error",responseCode ="500",content = @Content )
-				}
-			)
+	@Operation(summary = "Deleta um cliente", description = "Deleta um cliente", tags = { "Clientes" }, responses = {
+			@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized ", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content) })
 	@PreAuthorize("hasAnyAuthority('SCOPE_ADMIN', 'SCOPE_BASIC')")
-	public ResponseEntity<?> delete(@PathVariable Long id,JwtAuthenticationToken token ) {
-	var user= userRepository.findById( UUID.fromString(token.getName()));
-	var clibd=clienteRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Cliente de id "+id+"não encontrado na base de dados"));
-	var isAdmin=user.get().getRoles()
-			.stream()
-			.anyMatch(role-> role.getName().equalsIgnoreCase(Role.Values.ADMIN.name()));
-	if(isAdmin||clibd.getUser().getUserId().equals(UUID.fromString(token.getName()))) {
-		clienteRepository.deleteById(id);
-	}else {
-		throw new ForbiddenAccesException("O usuário não tem permissão para realizar esta ação");
-	}
-	
-	return ResponseEntity.noContent().build();
+	public ResponseEntity<?> delete(@PathVariable Long id, JwtAuthenticationToken token) {
+		var user = userRepository.findById(UUID.fromString(token.getName()));
+		var clibd = clienteRepository.findById(id).orElseThrow(
+				() -> new ResourceNotFoundException("Cliente de id " + id + "não encontrado na base de dados"));
+		var isAdmin = user.get().getRoles().stream()
+				.anyMatch(role -> role.getName().equalsIgnoreCase(Role.Values.ADMIN.name()));
+		if (isAdmin || clibd.getUser().getUserId().equals(UUID.fromString(token.getName()))) {
+			clienteRepository.deleteById(id);
+		} else {
+			throw new ForbiddenAccesException("O usuário não tem permissão para realizar esta ação");
+		}
+
+		return ResponseEntity.noContent().build();
 	}
 }
