c518b655c1d61e09260e43fcf518aaff89f8cc2a
Thauan Santos <126874939+Thauan-S@users.noreply.github.com>
2024-04-27 16:38:14 -0300

    update hook personalizado useGetClients

--- project/hooks/useGetClients.js
+++ project/hooks/useGetClients.js
@@ -1,55 +1,51 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 function useGetClients() {
-  const [username, setUsername] = useState(" ");
+  const [username, setUsername] = useState("");
   const [clientes, setClientes] = useState(null);
-  var [page, setPage] = useState(1);
+  var [page, setPage] = useState(0);
   const [size, setSize] = useState(10);
-  const [direction, setDirection] = useState("asc");
-  const [getClients, setGetClients] = useState(() => {
-    return (p)=>{
-    axios
-      .get("http://localhost:80/api/clientes/v1", {
-        headers: {
-          Authorization: `Bearer ${window.localStorage.getItem("token")}`,
-        },
-        params: {
-          page: p,
-          size: size,
-          direction: direction,
-        },
-      })
-      .then((response) => {
-        console.log("Response", response.data);
-        setClientes(response.data._embedded.clienteVOList);
-      })
-      .catch((error) => {
-        console.error("Erro ao Listar os Clientes", error);
-      })
-    }
-  });
+  const [direction, setDirection] = useState("ASC");
+  
+  // const [getClients, setGetClients] = useState(() => {
+  //   return (p)=>{
+  //   axios
+  //     .get("http://localhost/api/clientes/v1", {
+  //       headers: {
+  //         Authorization: `${window.localStorage.getItem("token")}`,
+  //       },
+  //       params: {
+  //         page: p,
+  //         size: size,
+  //         direction: direction,
+  //       },
+  //     })
+  //     .then((response) => {
+  //       console.log("Response", response.data);
+  //       setClientes(response.data._embedded.clienteVOList);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Erro ao Listar os Clientes", error);
+  //     })
+  //   }
+  // });
   useEffect(() => {
     setUsername(window.localStorage.getItem("username"));
     axios
       .get("http://localhost:80/api/clientes/v1", {
         headers: {
           Authorization: `Bearer ${window.localStorage.getItem("token")}`,
-        },
-        params: {
-          page: page,
-          size: size,
-          direction: direction,
-        },
+        },params:{
+        page:page,
+        size:size,
+        direction:direction
+       }
       })
-      .then((response) => {
-        console.log("Response", response.data);
-        setClientes(response.data._embedded.clienteVOList);
-      })
-      .catch((error) => {
-        console.error("Erro ao Listar os Clientes", error);
-      });
-  }, []);
-  return { username, clientes, setClientes, setPage, page, getClients };
+      .then(response => {
+        setClientes(response.data.clientesList)
+   });
+  }, [page,size,direction]);
+  return { username, clientes, setPage, page };
 }
 
 export default useGetClients;
