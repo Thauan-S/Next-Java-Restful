37e8f9168eaca4f1b5fca4e161208a27c57f108c
Thauan Santos <126874939+Thauan-S@users.noreply.github.com>
2024-07-31 17:53:47 -0300

    Adicionado teste com JwtAuthenticationToken

--- Tropical/src/test/java/com/tropical/services/EmpresaServiceTest.java
+++ Tropical/src/test/java/com/tropical/services/EmpresaServiceTest.java
@@ -5,21 +5,28 @@ import com.tropical.model.*;
 import com.tropical.repository.EmpresaRepository;
 import com.tropical.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.*;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.webjars.NotFoundException;
 
 import java.math.BigDecimal;
+import java.time.Instant;
 import java.util.*;
+import java.util.stream.Collectors;
+
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -33,12 +40,17 @@ class EmpresaServiceTest {
     private EmpresaRepository empresaRepository;
     @Mock
     private UserRepository userRepository;
-
+    @Mock
+    private JwtAuthenticationToken token;
     @InjectMocks
     private EmpresaService empresaService;
 
     @Captor
     ArgumentCaptor<Long> IdArgumentCaptor;
+    @Captor
+            ArgumentCaptor<UUID>uuidArgumentCaptor;
+    User user;
+    Empresa empresa;
 
     @Captor
     ArgumentCaptor<PageRequest> PageRequestArgumentCaptor;
@@ -48,21 +60,30 @@ class EmpresaServiceTest {
         Cliente cliente=new Cliente(1L, "cliente", "telefone",  new Date(2002-11-17),  "cep");
         Role admin=new Role();
         admin.setRoleId(1L);
+        admin.setName(Role.Values.ADMIN.toString());
+        user=new User();
+        user.setUserId(UUID.randomUUID());
+        user.setUsername("user");
+        user.setRoles(Set.of(admin));
+        token = mock(JwtAuthenticationToken.class);
+        when(token.getName()).thenReturn(user.getUserId().toString());
+        //Role basic=new Role();
+        //basic.setRoleId(2L);
 
-        Role basic=new Role();
-        basic.setRoleId(2L);
 
-        Role empresaRole=new Role();
-        empresaRole.setRoleId(1L);
+        //Role empresaRole=new Role();
+        //empresaRole.setRoleId(1L);
 
-        Set<Role> roles= Set.of(admin,basic,empresaRole);
+//        Set<Role> roles= Set.of(admin,basic,empresaRole);
+        Set<Role> roles= Set.of(admin);
         User user=new User(UUID.randomUUID(),"username","password",cliente,roles);
         PacoteDeViagem pacoteDeViagem=new PacoteDeViagem(1L, "destino",  "descricao","categoria",  3, "imagem",
                 new BigDecimal(3000), new Empresa());
-        Empresa empresa=new Empresa(1L,"empresa","123","endereco",user, List.of(pacoteDeViagem));
+         empresa=new Empresa(1L,"empresa","123","endereco",user, List.of(pacoteDeViagem));
         Empresa empresa2=new Empresa(2L,"empresa2","1234","endereco2",user, List.of(pacoteDeViagem));
         List<Empresa>empresaList= Arrays.asList(empresa,empresa2);
         Page empresaPage=new PageImpl<>(empresaList, PageRequest.of(0,2),empresaList.size());
+        lenient().doReturn(Optional.of(user)).when(userRepository).findById(uuidArgumentCaptor.capture());
         lenient().doReturn(empresaPage).when(empresaRepository).findAll(PageRequestArgumentCaptor.capture());
         lenient().doReturn(Optional.of(empresa)).when(empresaRepository).findById(1L);
         //Devo usar o lenient sempre , pois ele indicará que irei utilizar todos os mocks, independente se eu estiver
@@ -135,13 +156,35 @@ class EmpresaServiceTest {
             empresaDto.setNomeEmpresa("NovoNome");
         ///var jwt= preciso estudar como obter um token para usar em meu método;
 
-        var outPut=empresaService.update(empresaDto,new JwtAuthenticationToken());
+       // var outPut=empresaService.update(empresaDto,new JwtAuthenticationToken());
 
             //Act
             //Assert
    }}
-//
-//    @Test
-//    void delete() {
-//    }
-}
+    @Nested
+    class deleteById{
+        @Test
+        @DisplayName("Should delete a company by id with authentication token When user Is Admin")
+        void shouldDeleteACompaByIdWithSuccessWhenUserIsAdming(){
+            //arrange
+            var admin=new Role();
+            admin.setName("admin");
+            admin.setRoleId(1L);
+            user.setRoles(Set.of(admin));
+            doReturn(Optional.of(user)).when(userRepository).findById(uuidArgumentCaptor.capture());
+            doReturn(Optional.of(empresa)).when(empresaRepository).findById(IdArgumentCaptor.capture());
+            //Act
+            var empresa =empresaRepository.findById(1L);
+            var outPut=empresaService.delete(1L,token);
+
+            //Assert
+assertEquals(ResponseEntity.noContent().build(),outPut);
+verify(empresaRepository,times(1)).deleteById(1L);
+            }
+
+
+
+
+        }
+    }
+
