4c47bea55d24de790b8e2512b8da1104bca8629d
Thauan Santos <126874939+Thauan-S@users.noreply.github.com>
2024-08-23 17:08:57 -0300

    test: finalização dos testes dos serviços

--- Tropical/src/test/java/com/tropical/services/TokenServiceTest.java
+++ Tropical/src/test/java/com/tropical/services/TokenServiceTest.java
@@ -1,4 +1,82 @@
+package com.tropical.services;
+
+import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
+
+import com.tropical.data.dto.LoginRequest;
+import com.tropical.model.Role;
+import com.tropical.model.User;
+import com.tropical.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+
+@ExtendWith(MockitoExtension.class)
 class TokenServiceTest {
-  
+    @Mock
+    UserRepository userRepository;
+    @Mock
+    JwtEncoder jwtEncoder;
+    @Mock
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+    @InjectMocks
+    TokenService tokenService;
+    @Captor
+    ArgumentCaptor<String>stringArgumentCaptor;
+    @Captor
+    ArgumentCaptor<CharSequence>charSequenceArgumentCaptor;
+
+    private LoginRequest loginRequest;
+    private User user;
+    private Role role;
+    private String jwtToken = "mockJwtToken";
+
+    @BeforeEach
+    void setUp(){
+        loginRequest=new LoginRequest("user","123");
+        role = new Role();
+        user = new User();
+        user.setUserId(UUID.randomUUID());
+        user.setUsername("username");
+        user.setPassword("encodedPassword");
+        user.setRoles(Set.of(role));
+    }
+    @Nested
+    class login{
+    @Test
+    void shouldLogInSuccessFully() {
+        //Arrange
+        doReturn(Optional.of(user)).when(userRepository).findByUsername(stringArgumentCaptor.capture());
+        doReturn(true).when(bCryptPasswordEncoder).matches(charSequenceArgumentCaptor.capture(),stringArgumentCaptor.capture());
+
+        // Mock the JwtEncoder.encode method
+        Jwt jwtMock = mock(Jwt.class);
+        doReturn(jwtToken).when(jwtMock).getTokenValue();
+        doReturn(jwtMock).when(jwtEncoder).encode(any(JwtEncoderParameters.class));
+
+        // Act
+        var outPut = tokenService.login(loginRequest);
+
+        // Assert
+        assertEquals(200, outPut.getStatusCodeValue());
+        assertEquals(jwtToken, outPut.getBody().accessToken());
+
+    }
+    }
 }
--- Tropical/src/test/java/com/tropical/services/UserServiceTest.java
+++ Tropical/src/test/java/com/tropical/services/UserServiceTest.java
@@ -1,7 +1,11 @@
 package com.tropical.services;
 
+import com.tropical.data.dto.ClientDto;
 import com.tropical.data.dto.CreateUserDto;
+import com.tropical.data.dto.EnterpriseDto;
 import com.tropical.exceptions.UserAlreadyExistsException;
+import com.tropical.model.Client;
+import com.tropical.model.Enterprise;
 import com.tropical.model.Role;
 import com.tropical.model.User;
 import com.tropical.repository.ClientRepository;
@@ -18,9 +22,13 @@ import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.testcontainers.shaded.org.checkerframework.checker.units.qual.C;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 import static org.mockito.Mockito.*;
@@ -41,20 +49,68 @@ class UserServiceTest {
     @InjectMocks
     private UserService userService;
     private User user;
+    private User userAdmin;
+    private User enterpriseUser;
+    private User clientUser;
     private Role role;
+    private Role basicRole;
+    private Role enterpriseRole;
+    private Role adminRole;
+    private ClientDto clientDto;
     private CreateUserDto createUserDto;
+    private CreateUserDto createAdminUserDto;
+    private EnterpriseDto enterpriseDto;
 
     @Captor
-    ArgumentCaptor<UUID>uuidArgumentCaptor;
+    ArgumentCaptor<UUID> uuidArgumentCaptor;
     @Captor
-    ArgumentCaptor<String>stringArgumentCaptor;
+    ArgumentCaptor<String> stringArgumentCaptor;
+    @Captor
+    ArgumentCaptor<User> userArgumentCaptor;
+    @Captor
+    ArgumentCaptor<CharSequence> charSequenceArgumentCaptor;
+    @Captor
+    ArgumentCaptor<Client> clientArgumentCaptor;
+    @Captor
+    ArgumentCaptor<Enterprise> enterpriseArgumentCaptor;
+
     @BeforeEach
     void setUp() {
+
+
         user = new User();
-        role=new Role();
+
+        userAdmin = new User();
+
+        role = new Role();
         role.setName(Role.Values.BASIC.name());
         role.setRoleId(Role.Values.BASIC.getRoleId());
-        createUserDto=new CreateUserDto("thau","123");
+
+        basicRole =new Role();
+        basicRole.setName(Role.Values.BASIC.name());
+        basicRole.setRoleId(Role.Values.BASIC.getRoleId());
+
+        enterpriseRole = new Role();
+        enterpriseRole.setRoleId(Role.Values.EMPRESA.getRoleId());
+        enterpriseRole.setName(Role.Values.EMPRESA.name());
+
+        adminRole = new Role();
+        adminRole.setName(Role.Values.ADMIN.name());
+        adminRole.setRoleId(Role.Values.ADMIN.getRoleId());
+
+        createUserDto = new CreateUserDto("thau", "123");
+        createAdminUserDto = new CreateUserDto("admin", "123");
+        enterpriseDto = new EnterpriseDto();
+        enterpriseUser = new User();
+        enterpriseUser.setUsername("enterprise");
+        enterpriseUser.setPassword("123");
+        enterpriseDto.setUser(enterpriseUser);
+
+        clientUser=new User();
+        clientUser.setUsername("client");
+        clientUser.setPassword("123");
+        clientDto=new ClientDto();
+        clientDto.setUser(clientUser);
     }
 
     @Nested
@@ -64,39 +120,92 @@ class UserServiceTest {
         void shouldCreateUser() {
             //Arrange
             doReturn(role).when(roleRepository).findByName(stringArgumentCaptor.capture());
-
             //Act
-            var outPut=userService.newUser(createUserDto);
+            var outPut = userService.newUser(createUserDto);
             //Assert
             assertNotNull(outPut);
         }
+
         @Test
         @DisplayName("Should throws a UserAlreadyExistsException when user exists")
         void shouldThrowsAException() {
             //Arrange
             doReturn(Optional.of(user)).when(userRepository).findByUsername(stringArgumentCaptor.capture());
             //Act & Assert
-            assertThrows(UserAlreadyExistsException.class,()-> userService.newUser(createUserDto));
+            assertThrows(UserAlreadyExistsException.class, () -> userService.newUser(createUserDto));
         }
     }
 
     @Nested
-    class createAdmin{
+    class createAdmin {
         @DisplayName("Should create a admin with success")
-    void createNewAdmin() {
+        @Test
+        void createNewAdmin() {
+            // Arrange
+            when(userRepository.findByUsername(stringArgumentCaptor.capture())).thenReturn(Optional.empty());
+            when(roleRepository.findByName(stringArgumentCaptor.capture())).thenReturn(adminRole);
+            when(bCryptPasswordEncoder.encode(stringArgumentCaptor.capture())).thenReturn("encodedPassword");
+
+            // Act
+            var output = userService.createNewAdmin(createAdminUserDto);
+
+            // Assert
+            verify(userRepository).save(userArgumentCaptor.capture());
+            User savedUser = userArgumentCaptor.getValue();
+
+            assertEquals(createAdminUserDto.username(), savedUser.getUsername());
+            assertEquals("encodedPassword", savedUser.getPassword());
+            assertEquals(Set.of(adminRole), savedUser.getRoles());
+            assertEquals(ResponseEntity.ok().build(), output);
+        }
+    }
+
+    @Nested
+    class createNewEnterprise {
+        @Test
+        void createNewEnterprise() {
+            //Arrange
+            when(userRepository.findByUsername(stringArgumentCaptor.capture())).thenReturn(Optional.empty());
+            when(roleRepository.findByName(stringArgumentCaptor.capture())).thenReturn(adminRole);
+            when(bCryptPasswordEncoder.encode(stringArgumentCaptor.capture())).thenReturn("encodedPassword");
+            //Act
+            var outPut = userService.createNewEnterprise(enterpriseDto);
+            //Assert
+            assertNotNull(outPut);
+            verify(enterpriseRepository, times(1)).save(enterpriseArgumentCaptor.capture());
+
+        }
 
     }
+
+    @Nested
+    class newClient {
+        @Test
+        @DisplayName("Should create a client with success")
+        void shouldCreateAclientWithSuccess() {
+            //Arrange
+            doReturn(basicRole).when(roleRepository).findByName(stringArgumentCaptor.capture());
+            doReturn(Optional.empty()).when(userRepository).findByUsername(stringArgumentCaptor.capture());
+            when(bCryptPasswordEncoder.encode(stringArgumentCaptor.capture())).thenReturn("encodedPassword");
+            //Act
+            var outPut=userService.newClient(clientDto);
+            //Assert
+            verify(clientRepository,times(1)).save(clientArgumentCaptor.capture());
+        }
+    }
+    @Nested
+    class findAll{
+        @Test
+        @DisplayName("Should returns a List of all Users with success")
+        void listUsers() {
+            //Arrange
+            doReturn(List.of(user)).when(userRepository).findAll();
+            //Act
+            var outPut=userService.listUsers();
+            //Assert
+            assertEquals(ResponseEntity.ok(List.of(user)),outPut);
+            verify(userRepository,times(1)).findAll();
+        }
     }
 
-    @Test
-    void createNewEnterprise() {
-    }
-
-    @Test
-    void newClient() {
-    }
-
-    @Test
-    void listUsers() {
-    }
 }
